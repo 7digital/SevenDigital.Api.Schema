2d2d2171a6b7eb11c3c59e99c0c249922450037c
Anthony Steele <Anthony.Steele@7digital.com>
2014-10-28 17:31:03 +0000

    Add hidden release files

---
+++ src/Schema.Integration.Tests/EndpointTests/Releases/ReleaseRecommendTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using SevenDigital.Api.Wrapper;
+using SevenDigital.Api.Schema.Releases;
+
+namespace SevenDigital.Api.Schema.Integration.Tests.EndpointTests.Releases
+{
+	[TestFixture]
+	public class ReleaseRecommendTests
+	{
+		[Test]
+		public async Task Can_hit_endpoint()
+		{
+			var request = Api<ReleaseRecommend>.Create
+				.ForReleaseId(155408)
+				.WithParameter("country", "GB");
+			var release = await request.Please();
+
+			Assert.That(release, Is.Not.Null);
+			Assert.That(release.RecommendedItems.Count, Is.GreaterThan(0));
+		}
+	}
+}
---
+++ src/Schema.Integration.Tests/EndpointTests/Releases/ReleaseTracksTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SevenDigital.Api.Wrapper;
+using SevenDigital.Api.Schema.Pricing;
+using SevenDigital.Api.Schema.Releases;
+
+namespace SevenDigital.Api.Schema.Integration.Tests.EndpointTests.Releases
+{
+	[TestFixture]
+	public class ReleaseTracksTests
+	{
+		[Test]
+		public async Task Can_hit_endpoint()
+		{
+			var request = Api<ReleaseTracks>.Create
+				.ForReleaseId(1996067);
+			var releaseTracks = await request.Please();
+
+			Assert.That(releaseTracks, Is.Not.Null);
+			Assert.That(releaseTracks.Tracks.Count, Is.EqualTo(16));
+			Assert.That(releaseTracks.Tracks.First().Title, Is.EqualTo("Never Gonna Give You Up"));
+			Assert.That(releaseTracks.Tracks.First().Price.Status, Is.EqualTo(PriceStatus.Available));
+
+			Assert.That(releaseTracks.Tracks.First().Download.Packages[0].Id, Is.EqualTo(2));
+			Assert.That(releaseTracks.Tracks.First().Download.Packages[0].Description, Is.EqualTo("standard"));
+			Assert.That(releaseTracks.Tracks.First().Download.Packages[0].Price.CurrencyCode, Is.EqualTo("GBP"));
+			Assert.That(releaseTracks.Tracks.First().Download.Packages[0].Price.SevendigitalPrice, Is.EqualTo(0.99));
+			Assert.That(releaseTracks.Tracks.First().Download.Packages[0].Price.RecommendedRetailPrice, Is.EqualTo(0.99));
+			Assert.That(releaseTracks.Tracks.First().Download.Packages[0].Formats[0].Id, Is.EqualTo((17)));
+			Assert.That(releaseTracks.Tracks.First().Download.Packages[0].Formats[0].Description, Is.EqualTo("MP3 320"));
+		}
+
+		[Test]
+		public async Task can_determine_if_a_track_is_free()
+		{
+			var request = Api<ReleaseTracks>.Create
+				.ForReleaseId(394123);
+			var releaseTracks = await request.Please();
+
+			Assert.That(releaseTracks, Is.Not.Null);
+			Assert.That(releaseTracks.Tracks.Count, Is.EqualTo(1));
+			Assert.That(releaseTracks.Tracks.First().Price.Status, Is.EqualTo(PriceStatus.Free));
+		}
+
+		[Test]
+		public async Task can_determine_if_a_track_is_available_separately()
+		{
+			var request = Api<ReleaseTracks>.Create
+				.ForReleaseId(1193196);
+			var releaseTracks = await request.Please();
+
+			Assert.That(releaseTracks, Is.Not.Null);
+			Assert.That(releaseTracks.Tracks.Count, Is.GreaterThanOrEqualTo(1));
+			Assert.That(releaseTracks.Tracks.First().Price.Status, Is.EqualTo(PriceStatus.UnAvailable));
+		}
+	}
+}
