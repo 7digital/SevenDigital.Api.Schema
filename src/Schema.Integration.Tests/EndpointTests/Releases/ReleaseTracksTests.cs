db04875818ec768a4ad545e3f8ad02ea4c199e90
Anthony Steele <anthony.steele@7digital.com>
2015-05-11 11:48:26 +0100

    Test can pass now due to other deploys

--- src/Schema.Integration.Tests/EndpointTests/Releases/ReleaseTracksTests.cs
+++ src/Schema.Integration.Tests/EndpointTests/Releases/ReleaseTracksTests.cs
@@ -83,7 +83,7 @@ namespace SevenDigital.Api.Schema.Integration.Tests.EndpointTests.Releases
 			Assert.That(track.Price.Status, Is.EqualTo(PriceStatus.Free));
 		}
 
-		[Test, Ignore("not ready yet")]
+		[Test]
 		public async Task can_determine_if_a_track_is_available_separately()
 		{
 			var request = _api.Create<ReleaseTracks>()
