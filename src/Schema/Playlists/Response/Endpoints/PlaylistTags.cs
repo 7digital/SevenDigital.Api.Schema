b121a95d595376ffb7f76ad527364548b39e30ca
Sophie Cooper <sophie.cooper@7digital.com>
2016-12-14 10:17:28 +0000

    Add ~/playlists/tags get endpoint

---
+++ src/Schema/Playlists/Response/Endpoints/PlaylistTags.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using SevenDigital.Api.Schema.Attributes;
+using SevenDigital.Api.Schema.OAuth;
+
+namespace SevenDigital.Api.Schema.Playlists.Response.Endpoints
+{
+	[Serializable]
+	[OAuthSigned]
+	[ApiEndpoint("playlists/tags")]
+	[XmlRoot("tags")]
+	public class PlaylistTags
+	{
+		[XmlElement("tag")]
+		public List<PlaylistTag> Tags { get; set; }
+	}
+}
