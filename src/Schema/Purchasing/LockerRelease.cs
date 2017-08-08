3db42e1935a076431c64a1b0519371996be714b8
Mia Filisch <mia.filisch@7digital.com>
2017-08-08 12:53:43 +0100

    Remove unused directives

--- src/Schema/Purchasing/LockerRelease.cs
+++ src/Schema/Purchasing/LockerRelease.cs
@@ -2,11 +2,7 @@
 using System.Collections.Generic;
 using System.Xml.Serialization;
 using SevenDigital.Api.Schema.Artists;
-using SevenDigital.Api.Schema.Attributes;
-using SevenDigital.Api.Schema.Packages;
-using SevenDigital.Api.Schema.ParameterDefinitions.Get;
 using SevenDigital.Api.Schema.Releases;
-using SevenDigital.Api.Schema.Tracks;
 
 namespace SevenDigital.Api.Schema.Purchasing
 {
